7149d8bed0f573af4a90b88d0ffb499202a75711
brenin <130005400+bren112@users.noreply.github.com>
2024-07-19 21:56:05 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,12 @@ import './home.css';
 function Home() {
   const [noticias, setNoticias] = useState([]);
   const [titulos, setTitulos] = useState([]);
+  const [colaboradores, setColaboradores] = useState([]); // State for colaboradores
 
   useEffect(() => {
     fetchNoticias();
     fetchTitulosNoticias();
+    fetchColaboradores(); // Fetch colaboradores
   }, []);
 
   const fetchNoticias = async () => {
@@ -40,11 +42,22 @@ function Home() {
     }
   };
 
+  const fetchColaboradores = async () => {
+    try {
+      const { data, error } = await supabase.from("colaboradores").select("nome, avatar");
+      if (error) {
+        throw error;
+      }
+      setColaboradores(data);
+    } catch (error) {
+      console.error("Erro ao buscar colaboradores:", error.message);
+    }
+  };
+
   return (
     <div className="home-container">
       <div className="subtitulo">
         <p id="sub">
-         
           {titulos.map((noticia, index) => (
             <span key={noticia.id} id="span">
               {index !== 0 && " | "}
@@ -56,12 +69,22 @@ function Home() {
       <br />
       <h2 id="title">Últimas Notícias</h2>
       <br />
-
       <div className="noticias-resumidas">
         {noticias.map((noticia) => (
           <NoticiaResumida key={noticia.id} noticia={noticia} />
         ))}
       </div>
+      <br />
+      <h2 id="title">Colaboradores</h2>
+      <div className="colaboradores">
+        {colaboradores.map((colaborador, index) => (
+          <div key={index} className="colaborador">
+            <img src={colaborador.avatar} alt={colaborador.nome} className="colaborador-avatar" />
+            <p className="colaborador-nome">{colaborador.nome}</p>
+          </div>
+        ))}
+      </div>
+      <br />
     </div>
   );
 }
